208310ec303e8ce408b9fb17bf6b9393aba8b864
Alessandro <alessandro5@live.com>
2024-12-26 00:18:58 -0300

    Big O 4 rules

--- funChallenge.js
+++ funChallenge.js
@@ -1,10 +1,15 @@
 function funChallenge(input){
-    let a = 10;
-    a = 50 + 3;
+    let a = 10; // O(1)
+    a = 50 + 3; // O(1)
 
-    for(let i = 0; i < input.length; i ++){
-        let stranger = true;
-        a++;
+    for(let i = 0; i < input.length; i++){ // O(n)
+        let stranger = true; // O(n)
+        a++; // O(n)
     }
-    return a;
+    return a; // O(1)
 }
+// Big O (3 + 4n) --> O(n)
+// Rule 1: Worst case
+// Rule 2: Remove Constants 
+// Rule 3: Different terms for inputs
+// Rule 4: Drop Non Dominants
--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'nemo', 'nigel', 'squirt', 'darla']
 const large = new Array(10000).fill('nemo')
 
 function findNemo(array) {
   let t0 = performance.now()
   for (let i = 0; i < array.length; i++){
+    console.log('running')
     if(array[i] === 'nemo'){
       console.log('Found NEMO!');
+      break;
     }
   }
   let t1 = performance.now();
